47c6bc301911ff3bcd29381e48a360e4b408c25e
AWFJ <awfj837@gmail.com>
2022-01-15 09:44:49 +0300

    fix score

--- Timber2.cpp
+++ Timber2.cpp
@@ -23,6 +23,15 @@ int main()
     spriteBackground.setTexture(textureBackground);
     spriteBackground.setPosition(0, 0);
 
+    Texture textureBee;
+    Sprite spriteBee;
+    textureBee.loadFromFile("graphics/bee.png");
+    spriteBee.setTexture(textureBee);
+    spriteBee.setPosition(0, 800);
+
+    bool beeActive = false;
+    float beeSpeed = 0.2f;
+
     Texture textureTree;
     Sprite spriteTree;
     textureTree.loadFromFile("graphics/tree.png");
@@ -71,17 +80,19 @@ int main()
     scoreText.setFillColor(Color::White);
     scoreText.setPosition(20, 20);
 
+    // TIME BAR ------------------------------------------------------
     Clock clock;
     RectangleShape timeBar;
     float timeBarStartWidth = 400;
     float timeBarHeight = 80;
     timeBar.setSize(Vector2f(timeBarStartWidth, timeBarHeight));
     timeBar.setFillColor(Color::Red);
-    timeBar.setPosition((1920 / 2) - timeBarStartWidth / 2, 900);
+    timeBar.setPosition((1920 / 2) - timeBarStartWidth / 2, 980);
 
     Time gameTimeTotal;
     float timeRemaining = 6.0f;
     float timeBarWidthPerSecond = timeBarStartWidth / timeRemaining;
+    // -------------------------------------TIME BAR-------------------
 
     // SOUNDS --------------------------------------------------------
     SoundBuffer chopBuffer;
@@ -133,6 +144,7 @@ int main()
         if (Keyboard::isKeyPressed(Keyboard::Return)) {
             paused = false;
             acceptInput = true;
+            timeRemaining = 6;
         }
         if (Keyboard::isKeyPressed(Keyboard::Space)) {
             paused = true;
@@ -143,18 +155,39 @@ int main()
             if (Keyboard::isKeyPressed(Keyboard::Right)) {
                 playerSide = side::RIGHT;
                 score++;
+                timeRemaining += (2 / score) + 0.15;
                 spritePlayer.setPosition(1200, 720);
+                acceptInput = false;
             }
             else if (Keyboard::isKeyPressed(Keyboard::Left)) {
                 playerSide = side::LEFT;
                 score++;
+                timeRemaining += (2 / score) + 0.15;
                 spritePlayer.setPosition(580, 720);
+                acceptInput = false;
             }
         }
 
         window.clear();
 
         if (!paused) {
+            Time dt = clock.restart();
+
+            timeRemaining -= dt.asSeconds();
+            timeBar.setSize(Vector2f(timeBarWidthPerSecond *
+                timeRemaining, timeBarHeight));
+
+            if (timeRemaining <= 0.0f) {
+                paused = true;
+                messageText.setString("Out of time!");
+
+                FloatRect textRect = messageText.getLocalBounds();
+                messageText.setOrigin(textRect.left +
+                    textRect.width / 2.0f,
+                    textRect.top +
+                    textRect.height / 2.0f);
+            }
+
             std::stringstream ss;
             ss << "Score = " << score;
             scoreText.setString(ss.str());
@@ -167,7 +200,9 @@ int main()
         window.draw(spriteTree);
         window.draw(spriteAxe);
         window.draw(spritePlayer);
+        window.draw(spriteBee);
         window.draw(scoreText);
+        window.draw(timeBar);
 
         if (paused) {
             window.draw(messageText);
